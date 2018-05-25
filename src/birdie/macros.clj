21375f43f5901844255d835b231c8cb241e26498
Clark Kampfe <clark.kampfe@gmail.com>
2018-05-25 00:23:29 -0500

    macro formatting

--- src/birdie/macros.clj
+++ src/birdie/macros.clj
@@ -12,15 +12,15 @@
 
 (defmacro int-to-4-bytes [n]
   `(let [buf# (new js/ArrayBuffer 4)
-        byte-view# (new js/Uint8Array buf#)
-        int-view# (new js/Int32Array buf#)]
+         byte-view# (new js/Uint8Array buf#)
+         int-view# (new js/Int32Array buf#)]
 
-    (aset int-view# 0 ~n)
+     (aset int-view# 0 ~n)
 
-    [(aget byte-view# 3)
-     (aget byte-view# 2)
-     (aget byte-view# 1)
-     (aget byte-view# 0)]))
+     [(aget byte-view# 3)
+      (aget byte-view# 2)
+      (aget byte-view# 1)
+      (aget byte-view# 0)]))
 
 (defmacro double-to-8-bytes [n]
   `(let [buf# (new js/ArrayBuffer 8)
